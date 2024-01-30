9fce6e6d5a6631f62dcc02ded2033b91005ddbf3
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2024-01-30 21:09:03 +0530

    basic route configured

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import morgan from "morgan"
 import { config } from "dotenv"
+import appRouter from "./routes/index.js";
 
 config()
 const app = express()
 
 app.use(express.json())
 
+// remove it in production 
+app.use(morgan('dev'))
+
+app.use('/app/v1', appRouter)
 
 export default app
