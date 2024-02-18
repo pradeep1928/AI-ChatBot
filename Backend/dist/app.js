46fe26bd72a8af02fb060d183cee298b3abfceed
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2024-02-18 20:26:40 +0530

    cors policy updated

--- Backend/dist/app.js
+++ Backend/dist/app.js
@@ -6,7 +6,18 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 config();
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+    origin: `${process.env.FRONDEND_LINK}`, credentials: true,
+    optionSuccessStatus: 200,
+    headers: true,
+    exposedHeaders: 'Set-Cookie',
+    credentials: true,
+    allowedHeaders: [
+        'Access-Control-Allow-Origin',
+        'Content-Type',
+        'Authorization'
+    ]
+}));                                      // "http://0.0.0.0:5173"
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.set("trust proxy", 1);
