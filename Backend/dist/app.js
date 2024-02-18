9af46d144c876f624ba85cbf5538f3bac2a79298
Pradeep Padmukhi <ppadmukhi28@gmail.com>
2024-02-18 20:00:35 +0530

    added backend base url

--- Backend/dist/app.js
+++ Backend/dist/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.set("trust proxy", 1);
 // remove it in production 
 app.use(morgan('dev'));
 app.use('/api/v1', appRouter);
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './context/AuthContext.tsx'
 import axios from 'axios'
 import { Toaster } from 'react-hot-toast'
  
-axios.defaults.baseURL = "http://localhost:5000/api/v1"
+axios.defaults.baseURL = process.env.BASE_ROUTE // || "http://localhost:5000/api/v1"
 axios.defaults.withCredentials = true
 
 const theme = createTheme({
